8b94ecafa108bcf92593afd1b5d5d53aa8715919
Caleb Carvalho <caleb.carvalho@gmail.com>
2013-11-09 16:09:34 +0000

    Add account class

---
+++ SelSharp/Account.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace SelSharp
+{
+	 public class Account
+  {
+    private decimal balance;
+	
+    public void Deposit(decimal amount)
+    {
+      balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+      balance -= amount;
+    }
+
+    public void TransferFunds(Account destination, decimal amount)
+    {
+    }
+
+    public decimal Balance
+    {
+      get { return balance; }
+    }
+  }
+}
+
