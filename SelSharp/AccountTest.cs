29cd6a8be8e80405f1878032ab4382efc90e714e
Caleb Carvalho <caleb.carvalho@gmail.com>
2013-11-11 04:57:34 +0000

    Refactor Account Test

--- SelSharp/AccountTest.cs
+++ SelSharp/AccountTest.cs
@@ -3,62 +3,55 @@ using System;
 namespace SelSharp
 {
 	 using NUnit.Framework;
-
+	
   [TestFixture]
   public class AccountTest
   {
+    Account source;
+    Account destination;
+
+    [SetUp]
+    public void Init()
+    {
+      source = new Account();
+      source.Deposit(200m);
+	  
+      destination = new Account();
+      destination.Deposit(150m);
+    }
+
     [Test]
     public void TransferFunds()
     {
-      Account source = new Account();
-      source.Deposit(200m);
-      
-      Account destination = new Account();
-      destination.Deposit(150m);
-
       source.TransferFunds(destination, 100m);
 	  
       Assert.AreEqual(250m, destination.Balance);
       Assert.AreEqual(100m, source.Balance);
     }
 
-	
-		[Test]
-		[ExpectedException(typeof(InsufficientFundsException))]
-	
-		public void TransferWithInsufficientFunds(){
-  		Account source = new Account();
-  		source.Deposit(200m);
-	
-  		Account destination = new Account();
-  		destination.Deposit(150m);
-	
-  		source.TransferFunds(destination, 300m);
-		}
+    [Test]
+    [ExpectedException(typeof(InsufficientFundsException))]
+    public void TransferWithInsufficientFunds()
+    {
+      source.TransferFunds(destination, 300m);
+    }
 
-		[Test]
-		[Ignore("Decide how to implement transaction management")]
+    [Test]
+    [Ignore("Decide how to implement transaction management")]
+    public void TransferWithInsufficientFundsAtomicity()
+    {
+      try
+      {
+        source.TransferFunds(destination, 300m);
+      }
+      catch(InsufficientFundsException expected)
+      {
+      }
 
-		public void TransferWithInsufficientFundsAtomicity()
-
-		{
-  		Account source = new Account();
-  		source.Deposit(200m);
-
-  		Account destination = new Account();
-  		destination.Deposit(150m);
-	
-  		try
-  		{
-  		source.TransferFunds(destination, 300m);
-  		}	
-  		catch(InsufficientFundsException expected)
-  		{
-  		}
-
-  		Assert.AreEqual(200m, source.Balance);
-  		Assert.AreEqual(150m, destination.Balance);
-		}
+      Assert.AreEqual(200m, source.Balance);
+      Assert.AreEqual(150m, destination.Balance);
+    }
   }
+
 }
 
